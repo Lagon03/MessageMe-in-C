7912ac5177f7d7d911d5b4e9a473b0a1103fe531
MightyLord <andir@hotmail.fr>
2018-02-07 14:54:26 +0100

    Init client

---
+++ client.c
@@ -0,0 +1,30 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <err.h>
+# include <errno.h>
+# include <signal.h>
+# include <string.h>
+
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <sys/un.h>
+# include <netdb.h>
+
+// ----------------------------------------------------------------------------
+//                                  END HEADERS
+// ----------------------------------------------------------------------------
+
+
+// Function used to send message to the server
+void send(char* data, int fdout)
+{
+}
+
+void connect()
+{
+}
+
+void recv()
+{
+}
